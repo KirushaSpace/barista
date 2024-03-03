2c7121e268a345183416435bf891c689541b46a9
Kirill Nikiforov <nikiforov.kira@gmail.com>
2024-03-03 15:55:01 +0500

    add  LoginPage

---
+++ client/src/components/LoginPage.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { withRouter } from "./withRouter";
+import axios from 'axios';
+import PropTypes from 'prop-types';
+
+async function loginUser(credentials) {
+    const formData = new FormData()
+    formData.append("username", credentials.username)
+    formData.append("password", credentials.password)
+    return axios.post('http://localhost:8000/user/login', formData).then((response) => response.data)
+}
+
+
+class Login extends React.Component {
+    constructor(props){
+        super(props)
+        this.handleSubmit = this.handleSubmit.bind(this)
+
+        this.state = {
+            username: "",
+            password: ""
+        }
+    }
+
+    timeout(delay) {
+        return new Promise( res => setTimeout(res, delay) );
+    }
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        const username = this.state.username
+        const password = this.state.password
+        const token = await loginUser({
+          username,
+          password
+        });
+        await this.props.setToken(token)
+        this.props.navigate('/courses')
+      }
+    render() {
+        return (
+            <div className="background">
+                <form className="main_form" onSubmit={this.handleSubmit}>
+                    <h1 className="form_title">Вход</h1>
+                    <div className="form_field">
+                        <input className="form_input" placeholder=" " type="text" required name="username"
+                            onChange={(e) => this.state.username = e.target.value}/>
+                        <label className="form_label">Логин</label>
+                    </div>
+                    <div className="form_field">
+                        <input className="form_input" placeholder=" " type="password" required name="password"
+                            onChange={(e) => this.state.password = e.target.value}/>
+                        <label className="form_label">Пароль</label>
+                    </div>
+                    <div className="sign-in">
+                        <button className="form_button" type="submit">Войти</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+}
+
+
+export default withRouter(Login);
